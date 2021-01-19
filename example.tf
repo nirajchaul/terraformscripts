ffe77dbaba128a48ed7f41737bb0b55baa7c1e50
niraj-chaul <niraj.chaul@gmail.com>
2021-01-19 02:04:49 -0600

    end of day

--- example.tf
+++ example.tf
@@ -8,14 +8,52 @@ terraform {
 }
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
+
+/*resource "aws_key_pair" "keyFromTerraform" {
+  key_name   = "keyFromTerraform"
+  public_key = file("DevOpsAutomationKeyTF.pem")
+}*/
+
+/*resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = ""
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}*/
+
+
+
+resource "aws_instance" "instanceFromTerraform" {
+  ami           = "ami-0be2609ba883822ec"
   instance_type = var.instance_type
+  #security_groups = ["sg-a9cb348d"]
+  #key_name      = aws_key_pair.keyFromTerraform.key_name
+  user_data     = "${file("install_apache.sh")}"
+  
+
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
   default = "t2.micro"
 }
